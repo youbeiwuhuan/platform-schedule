a1e12af458b5700fc0839784984d2f6356f74d95
zhangyong <zhangyong7120180@163.com>
2019-08-04 10:09:57 +0800

    控制台

---
+++ schedule-console/src/main/java/com/courage/platform/schedule/console/core/exception/XxlJobException.java
@@ -0,0 +1,14 @@
+package com.courage.platform.schedule.console.core.exception;
+
+/**
+ * @author xuxueli 2019-05-04 23:19:29
+ */
+public class XxlJobException extends RuntimeException {
+
+    public XxlJobException() {
+    }
+    public XxlJobException(String message) {
+        super(message);
+    }
+
+}
