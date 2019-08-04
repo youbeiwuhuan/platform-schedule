2bcce1b6441b4f5ed79cc3adb02d80ea48e5f67a
zhangyong <zhangyong7120180@163.com>
2019-08-04 10:20:05 +0800

    控制台 主函数

--- schedule-console/src/main/java/com/courage/platform/schedule/console/MainApplication.java
+++ schedule-console/src/main/java/com/courage/platform/schedule/console/MainApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
- * @author xuxueli 2018-10-28 00:38:13
+ * 控制台主函数
  */
 @SpringBootApplication
 public class MainApplication {
