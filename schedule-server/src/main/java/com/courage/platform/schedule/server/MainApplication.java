88180cc5576be4bfb6c576739b7b9b58ec0263e1
zhangyong <zhangyong7120180@163.com>
2019-10-01 21:28:31 +0800

    去掉console 后续用来执行

---
+++ schedule-server/src/main/java/com/courage/platform/schedule/server/MainApplication.java
@@ -0,0 +1,13 @@
+package com.courage.platform.schedule.server;
+
+/**
+ * 任务调度主函数
+ * Created by zhangyong on 2019/10/1.
+ */
+public class MainApplication {
+
+    public static void main(String[] args) {
+        System.out.println("mainApplication");
+    }
+
+}
