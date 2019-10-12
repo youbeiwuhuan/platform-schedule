bc5120c977b52dead93bb80bf71c6b03c46b6592
zhangyong <zhangyong7120180@163.com>
2019-10-12 08:59:25 +0800

    耗时 毫秒

--- schedule-server/src/main/java/com/courage/platform/schedule/server/MainApplication.java
+++ schedule-server/src/main/java/com/courage/platform/schedule/server/MainApplication.java
@@ -1,16 +1,21 @@
 package com.courage.platform.schedule.server;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * 任务调度主函数
  * Created by zhangyong on 2019/10/1.
  */
 public class MainApplication {
 
+    private final static Logger logger = LoggerFactory.getLogger(MainApplication.class);
+
     public static void main(String[] args) throws InterruptedException {
-        while (true) {
-            System.out.println("mainApplication");
-            Thread.sleep(10000);
-        }
+        long start = System.currentTimeMillis();
+        logger.info("开始启动任务调度服务");
+
+        logger.info("结束启动任务调度服务,耗时:" + (System.currentTimeMillis() - start) + "ms");
     }
 
 }
