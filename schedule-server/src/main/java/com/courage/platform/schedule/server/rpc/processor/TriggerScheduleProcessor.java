519f8ac8aea68687fe2790d46022d379021939d5
zhangyong <zhangyong7120180@163.com>
2019-10-30 14:02:04 +0800

    任务调度处理器

--- schedule-server/src/main/java/com/courage/platform/schedule/server/rpc/processor/TriggerScheduleProcessor.java
+++ schedule-server/src/main/java/com/courage/platform/schedule/server/rpc/processor/TriggerScheduleProcessor.java
@@ -9,9 +9,9 @@ import org.slf4j.LoggerFactory;
 /*
     触发调度处理器
  */
-public class TriggerProcessor implements PlatformNettyRequestProcessor {
+public class TriggerScheduleProcessor implements PlatformNettyRequestProcessor {
 
-    private final static Logger logger = LoggerFactory.getLogger(TriggerProcessor.class);
+    private final static Logger logger = LoggerFactory.getLogger(TriggerScheduleProcessor.class);
 
     @Override
     public PlatformRemotingCommand processRequest(ChannelHandlerContext channelHandlerContext, PlatformRemotingCommand platformRemotingCommand) throws Exception {
