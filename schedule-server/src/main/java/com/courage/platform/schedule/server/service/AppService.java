c806bcbd1327fa5bd8b7d5d459cc81b798cdce35
zhangyong <zhangyong7120180@163.com>
2019-10-31 21:42:21 +0800

    spring app 信息

--- schedule-server/src/main/java/com/courage/platform/schedule/server/service/AppService.java
+++ schedule-server/src/main/java/com/courage/platform/schedule/server/service/AppService.java
@@ -1,7 +1,9 @@
 package com.courage.platform.schedule.server.service;
 
+import com.courage.platform.schedule.dao.AppinfoDao;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
@@ -14,6 +16,9 @@ import javax.annotation.PostConstruct;
 public class AppService {
 
     private final static Logger logger = LoggerFactory.getLogger(AppService.class);
+
+    @Autowired
+    private AppinfoDao appinfoDao;
 
     @PostConstruct
     public void post() {
