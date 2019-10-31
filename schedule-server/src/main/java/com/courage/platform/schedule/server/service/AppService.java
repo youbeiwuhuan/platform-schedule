51cba1d0182a62bab9b38fa68216d00424ae1ac8
zhangyong <zhangyong7120180@163.com>
2019-10-31 16:18:13 +0800

    应用管理信息

--- schedule-server/src/main/java/com/courage/platform/schedule/server/service/AppService.java
+++ schedule-server/src/main/java/com/courage/platform/schedule/server/service/AppService.java
@@ -4,6 +4,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.PostConstruct;
+
 /**
  * 应用管理
  * Created by zhangyong on 2019/10/31.
@@ -12,6 +14,10 @@ import org.springframework.stereotype.Service;
 public class AppService {
 
     private final static Logger logger = LoggerFactory.getLogger(AppService.class);
-
+    
+    @PostConstruct
+    public void post() {
+        logger.info("加载应用信息");
+    }
 
 }
