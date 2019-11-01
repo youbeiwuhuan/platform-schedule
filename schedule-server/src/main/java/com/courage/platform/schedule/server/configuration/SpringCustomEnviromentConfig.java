46b91cded7b3721619d26287afba596295891b2f
zhangyong <zhangyong7120180@163.com>
2019-11-01 22:23:46 +0800

    my time

--- schedule-server/src/main/java/com/courage/platform/schedule/server/configuration/SpringCustomEnviromentConfig.java
+++ schedule-server/src/main/java/com/courage/platform/schedule/server/configuration/SpringCustomEnviromentConfig.java
@@ -2,15 +2,33 @@ package com.courage.platform.schedule.server.configuration;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.config.PropertyPlaceholderConfigurer;
+import org.springframework.core.io.Resource;
+import org.springframework.util.DefaultPropertiesPersister;
+import org.springframework.util.PropertiesPersister;
+
+import java.io.IOException;
+import java.util.Properties;
 
 /**
  * 模仿springboot的各种环境配置信息
  * Created by zhangyong on 2019/11/1.
  */
-public class SpringCustomEnviromentConfig {
+public class SpringCustomEnviromentConfig extends PropertyPlaceholderConfigurer {
 
     private static final Logger logger = LoggerFactory.getLogger(SpringCustomEnviromentConfig.class);
 
+    protected Properties localProperties;
 
+    protected boolean localOverride = false;
+
+    private Resource[] resources;
+
+    private PropertiesPersister propertiesPersister = new DefaultPropertiesPersister();
+
+    @Override
+    protected void loadProperties(Properties props) throws IOException {
+        logger.info("pros:" + props);
+    }
 
 }
