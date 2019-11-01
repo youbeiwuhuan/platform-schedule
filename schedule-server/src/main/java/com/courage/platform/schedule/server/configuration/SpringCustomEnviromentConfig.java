8f5b2a03a46ae710aadf0f6fd6af0e86bdc3b48d
zhangyong <zhangyong7120180@163.com>
2019-11-01 21:49:31 +0800

    spring costom

---
+++ schedule-server/src/main/java/com/courage/platform/schedule/server/configuration/SpringCustomEnviromentConfig.java
@@ -0,0 +1,16 @@
+package com.courage.platform.schedule.server.configuration;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * 模仿springboot的各种环境配置信息
+ * Created by zhangyong on 2019/11/1.
+ */
+public class SpringCustomEnviromentConfig {
+
+    private static final Logger logger = LoggerFactory.getLogger(SpringCustomEnviromentConfig.class);
+
+
+
+}
