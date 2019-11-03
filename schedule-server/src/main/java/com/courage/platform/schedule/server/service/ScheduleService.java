5a72dd243b4c1200ea858f9eb14a9822ffc81b16
zhangyong <zhangyong7120180@163.com>
2019-11-03 14:08:04 +0800

    scheduleService

---
+++ schedule-server/src/main/java/com/courage/platform/schedule/server/service/ScheduleService.java
@@ -0,0 +1,16 @@
+package com.courage.platform.schedule.server.service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * 调度服务
+ * Created by zhangyong on 2019/11/3.
+ */
+public class ScheduleService {
+
+    private final static Logger logger = LoggerFactory.getLogger(ScheduleService.class);
+
+
+
+}
