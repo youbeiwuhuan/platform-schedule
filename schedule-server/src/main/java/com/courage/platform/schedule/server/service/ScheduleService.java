2a91f95fd58c6873978f1378f7a0a4fcca631188
zhangyong <zhangyong7120180@163.com>
2019-11-03 14:15:56 +0800

    分配任务信息

--- schedule-server/src/main/java/com/courage/platform/schedule/server/service/ScheduleService.java
+++ schedule-server/src/main/java/com/courage/platform/schedule/server/service/ScheduleService.java
@@ -2,15 +2,17 @@ package com.courage.platform.schedule.server.service;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
 
 /**
  * 调度服务
  * Created by zhangyong on 2019/11/3.
  */
+@Service
 public class ScheduleService {
 
     private final static Logger logger = LoggerFactory.getLogger(ScheduleService.class);
 
-
+    
 
 }
