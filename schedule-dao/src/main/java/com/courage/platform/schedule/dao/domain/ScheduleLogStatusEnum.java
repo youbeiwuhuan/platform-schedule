fd92591ddacfc85ecb79468281e454ad1c1fc26b
zhangyong <zhangyong7120180@163.com>
2019-11-04 22:02:13 +0800

    任务调度信息

---
+++ schedule-dao/src/main/java/com/courage/platform/schedule/dao/domain/ScheduleLogStatusEnum.java
@@ -0,0 +1,33 @@
+package com.courage.platform.schedule.dao.domain;
+
+/**
+ * Created by zhangyong on 2019/11/4.
+ */
+public enum ScheduleLogStatusEnum {
+
+    INITIALIZE(-1, "待处理"), SUCCESS(0, "成功"), FAIL(1, "失败"), PART_FAIL(2, "部分失败");
+
+    private Integer id;
+    private String name;
+
+    private ScheduleLogStatusEnum(Integer id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
--- schedule-server/src/main/java/com/courage/platform/schedule/server/service/ScheduleRpcService.java
+++ schedule-server/src/main/java/com/courage/platform/schedule/server/service/ScheduleRpcService.java
@@ -1,9 +1,11 @@
 package com.courage.platform.schedule.server.service;
 
 import com.courage.platform.schedule.core.util.IdGenerator;
+import com.courage.platform.schedule.core.util.IpUtil;
 import com.courage.platform.schedule.dao.ScheduleJobLogDao;
 import com.courage.platform.schedule.dao.domain.ScheduleJobInfo;
 import com.courage.platform.schedule.dao.domain.ScheduleJobLog;
+import com.courage.platform.schedule.dao.domain.ScheduleLogStatusEnum;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,13 +29,13 @@ public class ScheduleRpcService {
         scheduleJobInfo.setTriggerLastTime(new Date());
         //存储到db
         ScheduleJobLog scheduleJobLog = new ScheduleJobLog();
-        Long id = IdGenerator.getUniqueIdAutoSeq(1000);
+        Long id = IdGenerator.getUniqueIdAutoSeq(Math.abs(IpUtil.getIp().hashCode() % 1024));
         scheduleJobLog.setId(id);
         scheduleJobLog.setJobId(scheduleJobInfo.getId());
         scheduleJobLog.setAppId(scheduleJobInfo.getAppId());
         scheduleJobLog.setCreateTime(new Date());
         scheduleJobLog.setTriggerTime(new Date());
-        scheduleJobLog.setStatus(-1);
+        scheduleJobLog.setStatus(ScheduleLogStatusEnum.INITIALIZE.getId());
         scheduleJobLogDao.insert(scheduleJobLog);
 
         logger.info("开始执行:" + scheduleJobInfo.getJobName() + " param:" + scheduleJobInfo.getJobParam());
