6da682f230e2d966c36e43966d9a54ad744bfcd9
zhangyong <zhangyong7120180@163.com>
2019-11-07 09:45:05 +0800

    rpc 单元测试

---
+++ schedule-server/src/test/java/com/courage/platform/schedule/server/test/timer/RpcUnitTest.java
@@ -0,0 +1,13 @@
+package com.courage.platform.schedule.server.test.timer;
+
+/**
+ * rpc 单元测试
+ * Created by zhangyong on 2019/11/7.
+ */
+public class RpcUnitTest {
+
+    public static void main(String[] args) {
+
+    }
+
+}
