d5a18104c2924b6eb55509ac29217f48bb5e24f8
zhangyong <zhangyong7120180@163.com>
2019-11-07 10:12:47 +0800

    processor

--- schedule-server/src/main/java/com/courage/platform/schedule/server/rpc/processor/RegisterProcessor.java
+++ schedule-server/src/main/java/com/courage/platform/schedule/server/rpc/processor/RegisterProcessor.java
@@ -3,11 +3,16 @@ package com.courage.platform.schedule.server.rpc.processor;
 import com.courage.platform.rpc.remoting.netty.codec.PlatformNettyRequestProcessor;
 import com.courage.platform.rpc.remoting.netty.protocol.PlatformRemotingCommand;
 import io.netty.channel.ChannelHandlerContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
+ * 注册处理器
  * Created by zhangyong on 2019/11/7.
  */
 public class RegisterProcessor implements PlatformNettyRequestProcessor {
+
+    private final static Logger logger = LoggerFactory.getLogger(RegisterProcessor.class);
 
     @Override
     public PlatformRemotingCommand processRequest(ChannelHandlerContext channelHandlerContext, PlatformRemotingCommand platformRemotingCommand) throws Exception {
--- schedule-server/src/test/java/com/courage/platform/schedule/server/test/timer/RpcUnitTest.java
+++ schedule-server/src/test/java/com/courage/platform/schedule/server/test/timer/RpcUnitTest.java
@@ -8,6 +8,8 @@ public class RpcUnitTest {
 
     public static void main(String[] args) {
 
+
+
     }
 
 }
