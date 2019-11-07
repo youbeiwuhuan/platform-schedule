0361371ec7bbefd91dd436cf7e94849cd87c3a12
zhangyong <zhangyong7120180@163.com>
2019-11-07 09:57:15 +0800

    auth 信息

---
+++ schedule-server/src/main/java/com/courage/platform/schedule/server/rpc/RpcChannelManager.java
@@ -0,0 +1,17 @@
+package com.courage.platform.schedule.server.rpc;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+/**
+ * rpc 通道管理
+ * Created by zhangyong on 2019/11/7.
+ */
+@Component
+public class RpcChannelManager {
+
+    private static final Logger logger = LoggerFactory.getLogger(RpcChannelManager.class);
+
+
+}
--- schedule-server/src/main/java/com/courage/platform/schedule/server/rpc/package-info.java
+++
@@ -1,1 +0,0 @@
-package com.courage.platform.schedule.server.rpc;
