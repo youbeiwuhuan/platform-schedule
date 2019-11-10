ade435f7f655a00cbb9eddf85e34454e8be4df83
zhangyong <zhangyong7120180@163.com>
2019-11-10 16:03:15 +0800

    appinfo service

--- schedule-server/src/main/java/com/courage/platform/schedule/server/service/AppInfoService.java
+++ schedule-server/src/main/java/com/courage/platform/schedule/server/service/AppInfoService.java
@@ -4,10 +4,8 @@ import com.courage.platform.schedule.dao.AppinfoDao;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
-import javax.annotation.PostConstruct;
 import java.util.List;
 
 /**
