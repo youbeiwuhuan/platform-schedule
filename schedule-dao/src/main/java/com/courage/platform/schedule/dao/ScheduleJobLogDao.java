fe67e1d126b3a739aa22d97317468c501d8ad569
zhangyong <zhangyong7120180@163.com>
2019-11-10 15:50:28 +0800

    joblog dao

--- schedule-dao/src/main/java/com/courage/platform/schedule/dao/ScheduleJobLogDao.java
+++ schedule-dao/src/main/java/com/courage/platform/schedule/dao/ScheduleJobLogDao.java
@@ -3,10 +3,14 @@ package com.courage.platform.schedule.dao;
 import com.courage.platform.schedule.dao.domain.ScheduleJobLog;
 import org.springframework.stereotype.Repository;
 
+import java.util.Map;
+
 
 @Repository
 public interface ScheduleJobLogDao {
 
     Integer insert(ScheduleJobLog scheduleJobLog);
+
+    void update(Map map);
 
 }
--- schedule-server/src/main/java/com/courage/platform/schedule/server/rpc/processor/CallbackResultProcessor.java
+++ schedule-server/src/main/java/com/courage/platform/schedule/server/rpc/processor/CallbackResultProcessor.java
@@ -3,7 +3,7 @@ package com.courage.platform.schedule.server.rpc.processor;
 import com.alibaba.fastjson.JSON;
 import com.courage.platform.rpc.remoting.netty.codec.PlatformNettyRequestProcessor;
 import com.courage.platform.rpc.remoting.netty.protocol.PlatformRemotingCommand;
-import com.courage.platform.schedule.dao.ScheduleJobInfoDao;
+import com.courage.platform.schedule.dao.ScheduleJobLogDao;
 import com.courage.platform.schedule.rpc.protocol.CallbackCommand;
 import io.netty.channel.ChannelHandlerContext;
 import org.slf4j.Logger;
@@ -21,20 +21,20 @@ public class CallbackResultProcessor implements PlatformNettyRequestProcessor {
     private static final Logger logger = LoggerFactory.getLogger(CallbackResultProcessor.class);
 
     @Autowired
-    private ScheduleJobInfoDao scheduleJobInfoDao;
+    private ScheduleJobLogDao scheduleJobLogDao;
 
     @Override
     public PlatformRemotingCommand processRequest(ChannelHandlerContext channelHandlerContext, PlatformRemotingCommand platformRemotingCommand) throws Exception {
         byte[] bytes = platformRemotingCommand.getBody();
         CallbackCommand callbackCommand = JSON.parseObject(bytes, CallbackCommand.class);
-        
+
         //修改log状态
         Map map = new HashMap<>();
         map.put("id", callbackCommand.getJobLogId());
         map.put("message", callbackCommand.getHandleMsg());
         map.put("callbackTime", callbackCommand.getHandleTime());
         map.put("callbackStatus", callbackCommand.getHandleCode());
-        scheduleJobInfoDao.update(map);
+        scheduleJobLogDao.update(map);
         PlatformRemotingCommand response = new PlatformRemotingCommand();
         return response;
     }
