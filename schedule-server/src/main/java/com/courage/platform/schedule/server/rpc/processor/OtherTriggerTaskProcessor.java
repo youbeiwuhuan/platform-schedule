521039d9575521ad4a4f2843479288f59fc8e7d8
zhangyong <zhangyong7120180@163.com>
2019-11-11 11:55:56 +0800

    修改状态

--- schedule-server/src/main/java/com/courage/platform/schedule/server/rpc/processor/OtherTriggerTaskProcessor.java
+++ schedule-server/src/main/java/com/courage/platform/schedule/server/rpc/processor/OtherTriggerTaskProcessor.java
@@ -1,8 +1,35 @@
 package com.courage.platform.schedule.server.rpc.processor;
 
+import com.courage.platform.rpc.remoting.netty.codec.PlatformNettyRequestProcessor;
+import com.courage.platform.rpc.remoting.netty.protocol.PlatformRemotingCommand;
+import com.courage.platform.schedule.server.service.ScheduleTriggerService;
+import io.netty.channel.ChannelHandlerContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+
 /**
  * 其他方调用server 来实现自动执行
  * Created by zhangyong on 2019/11/11.
  */
-public class OtherTriggerTaskProcessor {
+public class OtherTriggerTaskProcessor implements PlatformNettyRequestProcessor {
+
+    private final static Logger logger = LoggerFactory.getLogger(OtherTriggerTaskProcessor.class);
+
+    @Autowired
+    private ScheduleTriggerService scheduleTriggerService;
+
+    @Override
+    public PlatformRemotingCommand processRequest(ChannelHandlerContext ctx, PlatformRemotingCommand request) throws Exception {
+        String jobId = null;
+        scheduleTriggerService.doRpcTrigger(Long.valueOf(jobId));
+        PlatformRemotingCommand response = new PlatformRemotingCommand();
+        return response;
+    }
+
+    @Override
+    public boolean rejectRequest() {
+        return false;
+    }
+
 }
