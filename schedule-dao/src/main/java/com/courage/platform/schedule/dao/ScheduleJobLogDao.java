38b7ae8c1f6a9617982a4b491f64c44ad68fa33c
zhangyong <zhangyong7120180@163.com>
2019-11-11 14:58:36 +0800

    dao 信息

--- schedule-dao/src/main/java/com/courage/platform/schedule/dao/ScheduleJobLogDao.java
+++ schedule-dao/src/main/java/com/courage/platform/schedule/dao/ScheduleJobLogDao.java
@@ -3,6 +3,7 @@ package com.courage.platform.schedule.dao;
 import com.courage.platform.schedule.dao.domain.ScheduleJobLog;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Map;
 
 
@@ -12,5 +13,9 @@ public interface ScheduleJobLogDao {
     Integer insert(ScheduleJobLog scheduleJobLog);
 
     void update(Map map);
+
+    List<ScheduleJobLog> findPage(Map map);
+
+    Integer count(Map map);
 
 }
