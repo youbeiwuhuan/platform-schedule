0e99d4c0ea763c922468631add2819f4e92d95a5
zhangyong <zhangyong7120180@163.com>
2019-11-13 10:51:14 +0800

    展示 callback & trigger 信息

--- schedule-console/src/main/webapp/static/js/joblog.index.1.js
+++ schedule-console/src/main/webapp/static/js/joblog.index.1.js
@@ -117,4 +117,13 @@ $(function () {
         joblogTable.fnDraw();
     });
 
+    logMessage = function (id) {
+        layer.open({
+            type: 1,
+            skin: 'layui-layer-rim', //加上边框
+            area: ['420px', '240px'], //宽高
+            content: 'html内容'
+        });
+    }
+
 });
