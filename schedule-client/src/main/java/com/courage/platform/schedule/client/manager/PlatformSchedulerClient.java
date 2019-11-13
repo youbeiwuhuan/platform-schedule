4e6284ae46014a187cc23785677d9c9439c8c306
zhangyong <zhangyong7120180@163.com>
2019-11-13 10:17:54 +0800

    client 每45s 获取信息

--- schedule-client/src/main/java/com/courage/platform/schedule/client/manager/PlatformSchedulerClient.java
+++ schedule-client/src/main/java/com/courage/platform/schedule/client/manager/PlatformSchedulerClient.java
@@ -71,7 +71,7 @@ public class PlatformSchedulerClient {
                     logger.error("scheduleAtFixedRate flush exception", e);
                 }
             }
-        }, 5000, 30000);
+        }, 5000, 45000);
     }
 
     public void destroy() {
