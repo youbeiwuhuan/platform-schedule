394cb189714e028b71132b66110b0093435a5175
zhangyong <zhangyong7120180@163.com>
2019-11-14 19:31:26 +0800

    delay 添加 lru cache 延迟 存储

---
+++ schedule-server/src/main/java/com/courage/platform/schedule/server/service/delaystore/DelayLruCache.java
@@ -0,0 +1,16 @@
+package com.courage.platform.schedule.server.service.delaystore;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+/**
+ * 简单的lru cache
+ * Created by zhangyong on 2019/11/14.
+ */
+@Component
+public class DelayLruCache {
+
+    private static final Logger logger = LoggerFactory.getLogger(DelayLruCache.class);
+
+}
---
+++ schedule-server/src/main/java/com/courage/platform/schedule/server/service/delaystore/DelayStore.java
@@ -0,0 +1,17 @@
+package com.courage.platform.schedule.server.service.delaystore;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+/**
+ * 延迟存储容器
+ * Created by zhangyong on 2019/11/14.
+ */
+@Component
+public class DelayStore {
+
+    private final static Logger logger = LoggerFactory.getLogger(DelayStore.class);
+
+
+}
