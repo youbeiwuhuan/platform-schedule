e34e77973dfd3a919450dcafc96eb62bfea8e3e9
zhangyong <zhangyong7120180@163.com>
2019-11-15 10:56:06 +0800

    console online app

--- schedule-server/src/main/java/com/courage/platform/schedule/server/rpc/processor/ConsoleOnlineAppProcessor.java
+++ schedule-server/src/main/java/com/courage/platform/schedule/server/rpc/processor/ConsoleOnlineAppProcessor.java
@@ -4,9 +4,11 @@ import com.alibaba.fastjson.JSON;
 import com.courage.platform.rpc.remoting.netty.codec.PlatformNettyRequestProcessor;
 import com.courage.platform.rpc.remoting.netty.protocol.PlatformRemotingCommand;
 import com.courage.platform.schedule.rpc.protocol.ConsoleOnlineAppCommand;
+import com.courage.platform.schedule.server.rpc.RpcChannelManager;
 import io.netty.channel.ChannelHandlerContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 
 /**
  * 在线应用查询处理器
@@ -16,11 +18,16 @@ public class ConsoleOnlineAppProcessor implements PlatformNettyRequestProcessor 
 
     private final static Logger logger = LoggerFactory.getLogger(ConsoleOnlineAppProcessor.class);
 
+    @Autowired
+    private RpcChannelManager rpcChannelManager;
+
     @Override
     public PlatformRemotingCommand processRequest(ChannelHandlerContext ctx, PlatformRemotingCommand request) throws Exception {
         byte[] bytes = request.getBody();
         ConsoleOnlineAppCommand consoleOnlineAppCommand = JSON.parseObject(bytes, ConsoleOnlineAppCommand.class);
-        return null;
+
+        PlatformRemotingCommand response = new PlatformRemotingCommand();
+        return response;
     }
 
     @Override
