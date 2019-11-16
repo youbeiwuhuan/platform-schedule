7fdf0a065de9fa7b072438fa782e74158078b62a
zhangyong <zhangyong7120180@163.com>
2019-11-16 19:48:24 +0800

    10000个 本地内存

--- schedule-server/src/main/java/com/courage/platform/schedule/server/service/delaystore/DelayLruCache.java
+++ schedule-server/src/main/java/com/courage/platform/schedule/server/service/delaystore/DelayLruCache.java
@@ -9,7 +9,7 @@ import com.google.common.cache.CacheBuilder;
  */
 public class DelayLruCache {
 
-    private static Cache loadingCache = CacheBuilder.newBuilder().maximumSize(50000).initialCapacity(5).build();
+    private static Cache loadingCache = CacheBuilder.newBuilder().maximumSize(10000).initialCapacity(5).build();
 
     public static void put(Long key, Object object) {
         loadingCache.put(key, object);
