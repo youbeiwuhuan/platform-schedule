1d0d8e78767098128628640bd90e730da345cf39
zhangyong <zhangyong7120180@163.com>
2019-11-16 19:53:31 +0800

    delay store

--- schedule-server/src/main/java/com/courage/platform/schedule/server/service/delaystore/DelayStore.java
+++ schedule-server/src/main/java/com/courage/platform/schedule/server/service/delaystore/DelayStore.java
@@ -2,15 +2,16 @@ package com.courage.platform.schedule.server.service.delaystore;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Component;
 
 /**
  * 延迟存储容器
  * Created by zhangyong on 2019/11/14.
  */
-@Component
 public class DelayStore {
 
     private final static Logger logger = LoggerFactory.getLogger(DelayStore.class);
 
+    public final String baseDir = System.getProperty("user.home");
+
+    
 }
