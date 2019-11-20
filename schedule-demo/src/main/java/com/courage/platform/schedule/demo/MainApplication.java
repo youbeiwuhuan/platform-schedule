66dcaec1c5e425aecec8a3212b533db93fced257
zhangyong <zhangyong7120180@163.com>
2019-11-20 18:03:08 +0800

    springboot 项目

---
+++ schedule-demo/src/main/java/com/courage/platform/schedule/demo/MainApplication.java
@@ -0,0 +1,12 @@
+package com.courage.platform.schedule.demo;
+
+/**
+ * Created by zhangyong on 2019/11/20.
+ */
+public class MainApplication {
+
+    public static void main(String[] args) {
+        
+    }
+
+}
