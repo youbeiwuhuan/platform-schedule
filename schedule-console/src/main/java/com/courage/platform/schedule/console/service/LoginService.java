8d48626ac5b6b43ed89d5beb8611449a2e328c15
zhangyong <zhangyong7120180@163.com>
2019-11-21 17:40:07 +0800

    login service

---
+++ schedule-console/src/main/java/com/courage/platform/schedule/console/service/LoginService.java
@@ -0,0 +1,18 @@
+package com.courage.platform.schedule.console.service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+/**
+ * 登录服务
+ * Created by zhangyong on 2019/11/21.
+ */
+@Service
+public class LoginService {
+
+    private final static Logger logger = LoggerFactory.getLogger(LoginService.class);
+
+
+
+}
