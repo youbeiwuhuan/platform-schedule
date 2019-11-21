78f3ccea7a543a47e78dd29c8d30894b0e5d0802
zhangyong <zhangyong7120180@163.com>
2019-11-21 10:08:08 +0800

    需要添加拦截器

--- schedule-console/src/main/java/com/courage/platform/schedule/console/mvc/WebMvcConfig.java
+++ schedule-console/src/main/java/com/courage/platform/schedule/console/mvc/WebMvcConfig.java
@@ -1,6 +1,7 @@
 package com.courage.platform.schedule.console.mvc;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
@@ -13,6 +14,10 @@ public class WebMvcConfig extends WebMvcConfigurerAdapter {
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         //将所有/static/** 访问都映射到classpath:/static/ 目录下
         registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
     }
 
 }
