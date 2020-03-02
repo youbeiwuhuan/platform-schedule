7eed3476caaf9991d4c33472e87c6c6ba3aea5ba
zhangyong <zhangyong7120180@163.com>
2020-03-02 14:13:14 +0800

    控制台 是否健康检查

--- schedule-console/src/main/java/com/courage/platform/schedule/console/service/HealthCheckService.java
+++ schedule-console/src/main/java/com/courage/platform/schedule/console/service/HealthCheckService.java
@@ -6,6 +6,7 @@ import com.courage.platform.schedule.dao.domain.PlatformNamesrv;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -32,6 +33,9 @@ public class HealthCheckService {
 
     @Autowired
     private ScheduleJobInfoService scheduleJobInfoService;
+
+    @Value("${console.healthCheck:true}")
+    private boolean healthCheck;
 
     @Transactional
     @Scheduled(initialDelay = 0, fixedRate = 20000)
