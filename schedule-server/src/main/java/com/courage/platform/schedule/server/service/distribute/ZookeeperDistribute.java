1de301656a8b2c9b9a57290610b01fc38cf3c4a0
zhangyong <zhangyong7120180@163.com>
2020-03-11 15:21:43 +0800

    添加zookeeper 处理

--- schedule-server/src/main/java/com/courage/platform/schedule/server/service/distribute/ZookeeperDistribute.java
+++ schedule-server/src/main/java/com/courage/platform/schedule/server/service/distribute/ZookeeperDistribute.java
@@ -1,13 +1,18 @@
 package com.courage.platform.schedule.server.service.distribute;
 
+import com.courage.platform.schedule.core.util.IpUtil;
+import com.courage.platform.schedule.dao.domain.PlatformNamesrv;
 import com.courage.platform.schedule.rpc.ScheduleRpcServer;
 import com.courage.platform.schedule.server.service.PlatformNamesrvService;
 import com.courage.platform.schedule.server.service.ScheduleJobExecutor;
 import com.courage.platform.schedule.server.service.ScheduleJobInfoService;
+import org.apache.commons.collections4.CollectionUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ZookeeperDistribute implements DistributeMode {
@@ -32,7 +37,22 @@ public class ZookeeperDistribute implements DistributeMode {
         //在/platform/schedule/servers/leader 节点下添加 若leader目录下存在节点 则监听节点 ，当前server状态是standby 若无节点，则做为leader ，作为任务分配的服务器
         long start = System.currentTimeMillis();
         logger.warn("开始启动zk任务分发器");
+        boolean hostAvailable = isCurrentHost();
         logger.warn("结束启动zk任务分发器,耗时:" + (System.currentTimeMillis() - start) + "ms");
+    }
+
+    private boolean isCurrentHost() {
+        String currentHost = IpUtil.getIpPort(scheduleRpcServer.localListenPort());
+        List<PlatformNamesrv> platformNamesrvList = platformNamesrvService.findAll();
+        if (CollectionUtils.isNotEmpty(platformNamesrvList)) {
+            for (PlatformNamesrv platformNamesrv : platformNamesrvList) {
+                if (currentHost.equals(platformNamesrv.getNamesrvIp())) {
+                    return true;
+                }
+            }
+        }
+        logger.warn("currentHost:" + currentHost + " isnot available , you should deploy to another container!");
+        return false;
     }
 
     @Override
