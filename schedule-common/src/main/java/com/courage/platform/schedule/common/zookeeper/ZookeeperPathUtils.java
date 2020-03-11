d5266ebe1fcac275a79a9520fda302f04243a4cc
zhangyong <zhangyong7120180@163.com>
2020-03-11 16:35:55 +0800

    zookeeper 节点 定义

--- schedule-common/src/main/java/com/courage/platform/schedule/common/zookeeper/ZookeeperPathUtils.java
+++ schedule-common/src/main/java/com/courage/platform/schedule/common/zookeeper/ZookeeperPathUtils.java
@@ -1,7 +1,27 @@
 package com.courage.platform.schedule.common.zookeeper;
 
+/**
+ * 存储结构：
+ *
+ * <pre>
+ * /platform
+ *     schedule
+ *        servers
+ *           running (EPHEMERAL)
+ *        leader
+ *           running (EPHEMERAL)
+ * </pre>
+ */
 public class ZookeeperPathUtils {
 
-    
+    public static final String ZOOKEEPER_SEPARATOR = "/";
+
+    public static final String PLATFORM_ROOT_NODE = ZOOKEEPER_SEPARATOR + "platform";
+
+    public static final String SCHEDULE_NODE = PLATFORM_ROOT_NODE + ZOOKEEPER_SEPARATOR + "schedule";
+
+    public static final String SCHEDULE_SERVER_NODE = SCHEDULE_NODE + ZOOKEEPER_SEPARATOR + "server";
+
+    public static final String LEADER_NODE = SCHEDULE_NODE + ZOOKEEPER_SEPARATOR + "leader";
 
 }
--- schedule-server/src/main/java/com/courage/platform/schedule/server/service/distribute/ZookeeperDistribute.java
+++ schedule-server/src/main/java/com/courage/platform/schedule/server/service/distribute/ZookeeperDistribute.java
@@ -1,5 +1,7 @@
 package com.courage.platform.schedule.server.service.distribute;
 
+import com.courage.platform.schedule.common.zookeeper.ZkClientx;
+import com.courage.platform.schedule.common.zookeeper.ZookeeperPathUtils;
 import com.courage.platform.schedule.core.util.IpUtil;
 import com.courage.platform.schedule.dao.domain.PlatformNamesrv;
 import com.courage.platform.schedule.rpc.ScheduleRpcServer;
@@ -31,27 +33,34 @@ public class ZookeeperDistribute implements DistributeMode {
     @Autowired
     private ScheduleRpcServer scheduleRpcServer;
 
+    @Autowired
+    private ZkClientx zkClientx;
+
     @Override
     public void start() {
         //核心思路是: 在zk /platform/schedule/servers 节点添加 127.0.0.1
         //在/platform/schedule/servers/leader 节点下添加 若leader目录下存在节点 则监听节点 ，当前server状态是standby 若无节点，则做为leader ，作为任务分配的服务器
         long start = System.currentTimeMillis();
         logger.warn("开始启动zk任务分发器");
-        boolean hostAvailable = isCurrentHost();
+        String currentHost = IpUtil.getIpPort(scheduleRpcServer.localListenPort());
+        boolean hostAvailable = isvalid(currentHost);
+        if (hostAvailable) {
+            //创建临时节点
+            zkClientx.createEphemeral(ZookeeperPathUtils.SCHEDULE_SERVER_NODE + ZookeeperPathUtils.ZOOKEEPER_SEPARATOR + IpUtil.getIpPort(scheduleRpcServer.localListenPort()), IpUtil.getIpPort(scheduleRpcServer.localListenPort()));
+        }
         logger.warn("结束启动zk任务分发器,耗时:" + (System.currentTimeMillis() - start) + "ms");
     }
 
-    private boolean isCurrentHost() {
-        String currentHost = IpUtil.getIpPort(scheduleRpcServer.localListenPort());
+    private boolean isvalid(String hostIp) {
         List<PlatformNamesrv> platformNamesrvList = platformNamesrvService.findAll();
         if (CollectionUtils.isNotEmpty(platformNamesrvList)) {
             for (PlatformNamesrv platformNamesrv : platformNamesrvList) {
-                if (currentHost.equals(platformNamesrv.getNamesrvIp())) {
+                if (hostIp.equals(platformNamesrv.getNamesrvIp())) {
                     return true;
                 }
             }
         }
-        logger.warn("currentHost:" + currentHost + " isnot available , you should deploy to another container!");
+        logger.warn("currentHost:" + hostIp + " isnot available , you should deploy to another container!");
         return false;
     }
 
