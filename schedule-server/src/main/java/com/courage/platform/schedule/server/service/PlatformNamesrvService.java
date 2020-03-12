124c0cdc4887a3a83e81dc4c6bf75da8570d5240
zhangyong <zhangyong7120180@163.com>
2020-03-12 13:13:25 +0800

    application 服务

--- schedule-server/src/main/java/com/courage/platform/schedule/server/service/PlatformNamesrvService.java
+++ schedule-server/src/main/java/com/courage/platform/schedule/server/service/PlatformNamesrvService.java
@@ -2,6 +2,9 @@ package com.courage.platform.schedule.server.service;
 
 import com.courage.platform.schedule.dao.PlatformNamesrvDao;
 import com.courage.platform.schedule.dao.domain.PlatformNamesrv;
+import org.apache.commons.collections4.CollectionUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,6 +16,8 @@ import java.util.List;
 @Service
 public class PlatformNamesrvService {
 
+    private final static Logger logger = LoggerFactory.getLogger(PlatformNamesrvService.class);
+
     @Autowired
     private PlatformNamesrvDao platformNamesrvDao;
 
@@ -22,6 +27,19 @@ public class PlatformNamesrvService {
 
     public List<PlatformNamesrv> findAll() {
         return platformNamesrvDao.findAll();
+    }
+
+    public boolean isValidHost(String hostIp) {
+        List<PlatformNamesrv> platformNamesrvList = platformNamesrvDao.findAll();
+        if (CollectionUtils.isNotEmpty(platformNamesrvList)) {
+            for (PlatformNamesrv platformNamesrv : platformNamesrvList) {
+                if (hostIp.equals(platformNamesrv.getNamesrvIp())) {
+                    return true;
+                }
+            }
+        }
+        logger.warn("currentHost:" + hostIp + " isnot available , you should deploy to another container!");
+        return false;
     }
 
 }
--- schedule-server/src/main/java/com/courage/platform/schedule/server/service/distribute/ZookeeperDistribute.java
+++ schedule-server/src/main/java/com/courage/platform/schedule/server/service/distribute/ZookeeperDistribute.java
@@ -3,18 +3,14 @@ package com.courage.platform.schedule.server.service.distribute;
 import com.courage.platform.schedule.common.zookeeper.ZkClientx;
 import com.courage.platform.schedule.common.zookeeper.ZookeeperPathUtils;
 import com.courage.platform.schedule.core.util.IpUtil;
-import com.courage.platform.schedule.dao.domain.PlatformNamesrv;
 import com.courage.platform.schedule.rpc.ScheduleRpcServer;
 import com.courage.platform.schedule.server.service.PlatformNamesrvService;
 import com.courage.platform.schedule.server.service.ScheduleJobExecutor;
 import com.courage.platform.schedule.server.service.ScheduleJobInfoService;
-import org.apache.commons.collections4.CollectionUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
 
 @Service
 public class ZookeeperDistribute implements DistributeMode {
@@ -38,30 +34,23 @@ public class ZookeeperDistribute implements DistributeMode {
 
     @Override
     public void start() {
-        //核心思路是: 在zk /platform/schedule/servers 节点添加 127.0.0.1
+        //核心思路是: 在zk /platform/schedule/servers 节点添加 127.0.0.1:12999
         //在/platform/schedule/servers/leader 节点下添加 若leader目录下存在节点 则监听节点 ，当前server状态是standby 若无节点，则做为leader ，作为任务分配的服务器
         long start = System.currentTimeMillis();
         logger.warn("开始启动zk任务分发器");
-        String currentHost = IpUtil.getIpPort(scheduleRpcServer.localListenPort());
-        boolean hostAvailable = isvalid(currentHost);
-        if (hostAvailable) {
-            //创建临时节点
-            zkClientx.createEphemeral(ZookeeperPathUtils.SCHEDULE_SERVER_NODE + ZookeeperPathUtils.ZOOKEEPER_SEPARATOR + IpUtil.getIpPort(scheduleRpcServer.localListenPort()), IpUtil.getIpPort(scheduleRpcServer.localListenPort()));
-        }
+
         logger.warn("结束启动zk任务分发器,耗时:" + (System.currentTimeMillis() - start) + "ms");
     }
 
-    private boolean isvalid(String hostIp) {
-        List<PlatformNamesrv> platformNamesrvList = platformNamesrvService.findAll();
-        if (CollectionUtils.isNotEmpty(platformNamesrvList)) {
-            for (PlatformNamesrv platformNamesrv : platformNamesrvList) {
-                if (hostIp.equals(platformNamesrv.getNamesrvIp())) {
-                    return true;
-                }
-            }
+    private void startZkService() {
+        String currentHost = IpUtil.getIpPort(scheduleRpcServer.localListenPort());
+        boolean hostAvailable = platformNamesrvService.isValidHost(currentHost);
+        //创建临时节点
+        if (!zkClientx.exists(ZookeeperPathUtils.SCHEDULE_SERVER_NODE)) {
+            zkClientx.createPersistent(ZookeeperPathUtils.SCHEDULE_SERVER_NODE, true);
         }
-        logger.warn("currentHost:" + hostIp + " isnot available , you should deploy to another container!");
-        return false;
+        String hostPath = ZookeeperPathUtils.SCHEDULE_SERVER_NODE + ZookeeperPathUtils.ZOOKEEPER_SEPARATOR + IpUtil.getIpPort(scheduleRpcServer.localListenPort());
+        zkClientx.createEphemeral(hostPath, IpUtil.getIpPort(scheduleRpcServer.localListenPort()));
     }
 
     @Override
