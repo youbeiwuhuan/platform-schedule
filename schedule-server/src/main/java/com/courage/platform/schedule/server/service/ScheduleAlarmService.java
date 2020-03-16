2721d38a3afbcb71ace119a46f9560cf4ef9bb12
zhangyong <zhangyong7120180@163.com>
2020-03-16 17:05:01 +0800

    调度 报警服务

---
+++ schedule-server/src/main/java/com/courage/platform/schedule/server/service/ScheduleAlarmService.java
@@ -0,0 +1,13 @@
+package com.courage.platform.schedule.server.service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/*
+  报警服务
+ */
+public class ScheduleAlarmService {
+
+    private final static Logger logger = LoggerFactory.getLogger(ScheduleAlarmService.class);
+
+}
