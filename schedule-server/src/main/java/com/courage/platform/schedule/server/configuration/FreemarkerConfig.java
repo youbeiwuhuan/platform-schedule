22abc21576476bacd8d0da7a442636fd3bb1afd8
zhangyong <zhangyong7120180@163.com>
2020-03-18 10:57:25 +0800

    模板引擎配置

---
+++ schedule-server/src/main/java/com/courage/platform/schedule/server/configuration/FreemarkerConfig.java
@@ -0,0 +1,13 @@
+package com.courage.platform.schedule.server.configuration;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * 模板配置
+ */
+public class FreemarkerConfig {
+
+    private static final Logger logger = LoggerFactory.getLogger(FreemarkerConfig.class);
+
+}
