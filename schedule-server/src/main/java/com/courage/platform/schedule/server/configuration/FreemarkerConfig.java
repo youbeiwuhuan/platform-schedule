089b9a2c53724e852f2f6656dd0d9ac230ef2b81
zhangyong <zhangyong7120180@163.com>
2020-03-18 11:30:50 +0800

    添加freemarker 的工具类  简单的

--- schedule-server/src/main/java/com/courage/platform/schedule/server/configuration/FreemarkerConfig.java
+++ schedule-server/src/main/java/com/courage/platform/schedule/server/configuration/FreemarkerConfig.java
@@ -1,13 +1,50 @@
 package com.courage.platform.schedule.server.configuration;
 
+import freemarker.template.Configuration;
+import freemarker.template.Template;
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.io.StringWriter;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
- * 模板配置
+ * freemarker模板配置
  */
+@Service
 public class FreemarkerConfig {
 
     private static final Logger logger = LoggerFactory.getLogger(FreemarkerConfig.class);
+
+    private Configuration configuration;
+
+    public FreemarkerConfig() throws IOException {
+        this.configuration = new Configuration(Configuration.VERSION_2_3_30);
+        this.configuration.setDefaultEncoding("utf-8");
+        this.configuration.setClassLoaderForTemplateLoading(ClassLoader.getSystemClassLoader(), "template");
+        this.configuration.setDateTimeFormat("yyyy-MM-dd HH:mm:ss");
+    }
+
+    public String doTemplate(String templateName, Map<String, Object> param) {
+        StringWriter writer = new StringWriter();
+        try {
+            Template template = this.configuration.getTemplate(templateName);
+            template.process(param, writer);
+            return writer.toString();
+        } catch (Exception e) {
+            logger.error("doTemplate error:", e);
+        }
+        return StringUtils.EMPTY;
+    }
+
+    public static void main(String[] args) throws IOException {
+        FreemarkerConfig freemarkerConfig = new FreemarkerConfig();
+        String rtn = freemarkerConfig.doTemplate("alarmMail.ftl", new HashMap<>());
+        System.out.println(rtn);
+    }
 
 }
