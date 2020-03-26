7c5de81d73016fa829d278071e9a7d0b1b1a8a18
zhangyong <zhangyong7120180@163.com>
2020-03-26 11:19:48 +0800

    rocksdb 单元测试

--- schedule-server/src/test/java/com/courage/platform/schedule/server/test/timer/RockdbUnitTest.java
+++ schedule-server/src/test/java/com/courage/platform/schedule/server/test/timer/RockdbUnitTest.java
@@ -1,13 +1,34 @@
 package com.courage.platform.schedule.server.test.timer;
 
+import org.junit.Before;
 import org.junit.Test;
+import org.rocksdb.Options;
+import org.rocksdb.RocksDB;
+import org.rocksdb.RocksDBException;
+
+import java.io.UnsupportedEncodingException;
 
 public class RockdbUnitTest {
 
-    @Test
-    public void scan() {
+    static {
+        RocksDB.loadLibrary();
+    }
 
-        
+    private RocksDB rocksDB;
+
+    @Before
+    public void before() throws RocksDBException {
+        Options options = new Options();
+        options.setCreateIfMissing(true);
+        this.rocksDB = RocksDB.open(options, "D:\\test");
+    }
+
+    @Test
+    public void scan() throws UnsupportedEncodingException, RocksDBException {
+        this.rocksDB.put("hello".getBytes(), "张勇".getBytes("UTF-8"));
+        byte[] bytes = this.rocksDB.get("hello".getBytes());
+        String value = new String(bytes, "UTF-8");
+        System.out.println(value);
     }
 
 }
