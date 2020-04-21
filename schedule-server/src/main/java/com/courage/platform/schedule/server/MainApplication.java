01ef9436c1570367095ce3419378019fac596b0f
zhangyong <zhangyong7120180@163.com>
2020-04-21 17:14:44 +0800

    修改主程序

--- schedule-server/src/main/java/com/courage/platform/schedule/server/MainApplication.java
+++ schedule-server/src/main/java/com/courage/platform/schedule/server/MainApplication.java
@@ -16,16 +16,6 @@ public class MainApplication {
         long start = System.currentTimeMillis();
         logger.info("开始启动任务调度服务");
         final ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext("classpath:spring-app.xml");
-        Runtime.getRuntime().addShutdownHook(new Thread(new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    applicationContext.close();
-                } catch (Throwable e) {
-                    logger.error("applicationContext close error:", e);
-                }
-            }
-        }));
         logger.info("结束启动任务调度服务,耗时:" + (System.currentTimeMillis() - start) + "ms");
     }
     
